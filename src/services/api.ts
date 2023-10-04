9b88c20aac4c8de693bf64789adec7ed1e8ea8c7
Marcos Vinicius <82465988+marcosviniciusjau@users.noreply.github.com>
2023-10-04 17:53:45 -0300

    Delete src/services/api.ts

--- src/services/api.ts
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-const apiKey = "036386b34f954747a739577b5bcb77d3"
-
-export const api = axios.create({
-  baseURL: "https://api.clarifai.com",
-  headers: {
-    Authorization: `Bearer ${apiKey}`,
-  },
-})
